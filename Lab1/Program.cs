c1e57312e548f881bae33ac0ebbebbf4acb13a78
Jnowak-CCC <snootclaws89@gmail.com>
2025-03-22 17:56:20 +0000

    lab1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+     string name = "Jesse"
+      int favNum = 64
+      string favLang = "python"
+      double progsBefore = 6
+      bool hasEXP = true
+      if hasEXP == true:
+        string exp = "has"
+      else:
+        string exp = "does not have"
+      Console.WriteLine(name + "'s favorite number is " + favNum + ". " + name + "'s favorite  programming language is " + favLang + name + "has written " + progsBefore +" programs" + " and " + exp + " programming experience")
     }
 }
