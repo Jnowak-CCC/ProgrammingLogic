988e07f3425b4c2c57319f89b86055ab76d9f473
Jnowak-CCC <snootclaws89@gmail.com>
2025-05-04 15:23:21 +0000

    do Lab7

--- Lab6/obj/Debug/net8.0/Lab6.AssemblyInfo.cs
+++ Lab6/obj/Debug/net8.0/Lab6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b44891feacc9f92fb70b3053685609ee51f3090")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+995245a4bd703262c625e6482a619e17ab4f2aa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab7/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+class Book
+{
+    private string title;
+    private string author;
+
+    // Default Constructor
+    public Book()
+    {
+        title = "Unknown Title";
+        author = "Unknown Author";
+    }
+
+    // Parameterized Constructor (1 parameter)
+    public Book(string title)
+    {
+        this.title = title;
+        this.author = "Unknown Author";
+    }
+
+    // Overloaded Constructor (2 parameters)
+    public Book(string title, string author)
+    {
+        this.title = title;
+        this.author = author;
+    }
+
+   
+    public string GetTitle()
+    {
+        return title;
+    }
+
+    
+    public void SetTitle(string newTitle)
+    {
+        title = newTitle;
+    }
+
+   
+    public string GetAuthor()
+    {
+        return author;
+    }
+
+    
+    public void SetAuthor(string newAuthor)
+    {
+        author = newAuthor;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Book book1 = new Book();
+        Console.WriteLine(book1.GetTitle());  
+
+        book1.SetTitle("C# Fundamentals");
+        Console.WriteLine(book1.GetTitle());
+
+       
+        Book book2 = new Book("Advanced C#");
+        Console.WriteLine(book2.GetTitle());  
+
+       
+        Book book3 = new Book("Advanced C#", "John Doe");
+        Console.WriteLine("Title: " + book3.GetTitle());   
+        Console.WriteLine("Author: " + book3.GetAuthor());
+
+       
+        Book book4 = new Book();
+        book4.SetTitle("Mastering C#");
+        book4.SetAuthor("Jane Smith");
+        Console.WriteLine("Title: " + book4.GetTitle());  
+        Console.WriteLine("Author: " + book4.GetAuthor()); 
+    }
+}
